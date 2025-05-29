485f97010818cf679ead1d9ee4c7a2961f1a5add
Maryna <doctor.who2016.dw@gmail.com>
2025-05-29 17:48:43 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,7 +3,8 @@ import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
   <div className="movies">
-    {movies.length !== 0 &&
-      movies.map(movie => <MovieCard movie={movie} key={movie.imdbId} />)}
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
   </div>
 );
